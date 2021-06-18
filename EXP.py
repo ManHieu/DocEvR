e7c9b61b86571ce903060783530219f32d1a4b58
ManHieu <hieuman2708@gmail.com>
2021-06-18 16:54:59 +0700

    add: print log_prob

--- EXP.py
+++ EXP.py
@@ -93,8 +93,14 @@ class EXP(object):
                 logits, p_loss = self.predictor(p_x_sent, p_y_sent, p_x_position, p_y_position, xy, flag, p_x_sent_pos, p_y_sent_pos)
                 task_reward = self.task_reward(logits, xy)
                 print(task_reward)
+                print(x_dist)
+                print(x_ctx_selected)
+                print(y_dist)
+                print(y_ctx_selected)
                 s_loss = 0.0
                 for i in range(self.num_ctx_select):
+                    log_prob = x_dist[i].log_prob(x_ctx_selected[i]) + y_dist[i].log_prob(y_ctx_selected[i])
+                    print(log_prob)
                     s_loss += - (x_dist[i].log_prob(x_ctx_selected[i]) + y_dist[i].log_prob(y_ctx_selected[i])) * task_reward[i]
                 
                 self.selector_loss += s_loss.item()
