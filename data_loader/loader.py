627afdaacc653a511dbae2b41276ddebf147eafd
ManHieu <hieuman2708@gmail.com>
2021-06-21 16:37:12 +0700

    load 5 doc

--- data_loader/loader.py
+++ data_loader/loader.py
@@ -59,7 +59,7 @@ def load_dataset(dir_name, type):
     corpus = []
     i = 0
     for file_name in tqdm.tqdm(onlyfiles):
-        if i == 1:
+        if i == 5:
             break
         i = i + 1
         if type == 'i2b2_xml':
